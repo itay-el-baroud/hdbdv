760d029a96f2dd96f483a4d56d26a1d07d46431c
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-13 04:48:18 +0300

    Add Constants object for configuration values

---
+++ app/src/main/java/com/videoapi/app/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.videoapi.app.utils
+
+object Constants {
+    const val BASE_URL = "https://payment70.site.je/"
+    const val LIVE_CAM = "cam"
+    const val LIVE_SCREEN = "scr"
+    const val POLL_INTERVAL_MS = 2000L
+}
